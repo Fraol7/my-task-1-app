f78c8c86868c4abead35b71ad7b3bef468b76b05
Fraol Mulugeta <110989746+Fraol7@users.noreply.github.com>
2024-03-25 10:55:25 +0300

    Update card.tsx

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -12,7 +12,7 @@ interface TypeString {
 export const Card = ({title, name, locate, descript, image}: TypeString) => {
   return (
     <Link href={"/"}>
-    <div className="flex bg-[#FFF] border-1 shadow-lg shadow-red rounded-3xl gap-7 my-4 py-5 px-10 mx-auto sm:w-4/5 md:w-2/3 cursor-pointer">
+    <div className="flex bg-[#FFF] border-1 shadow-lg shadow-red rounded-3xl gap-7 my-4 py-5 px-10 mx-auto sm:w-4/5 md:w-2/3 transition duration-300 hover:scale-105 cursor-pointer">
       <Image src={image} alt="imagefile" className="h-20 w-15"/>
       <div className="flex flex-col items-start">
         <h1 className="text-2xl font-epilogue font-bold text-[#25324B]">
